0623af982bbd51aff2914bb61991ceee1ab38822
Nirbhik01 <nirbhikdhakal8@gmail.com>
2026-04-09 17:04:38 +0545

    (feat): frontend and backend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return (
+    <p className="text-4xl text-blue-800 font-bold">
+      Tailwind working sdfh
+    </p>
+  )
+}
