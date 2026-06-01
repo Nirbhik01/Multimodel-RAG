350f4399cbc688c3b52b02570f574a2d4f970efa
Nirbhik01 <nirbhikdhakal8@gmail.com>
2026-06-01 11:34:13 +0545

    (refactor): increased the number of retrieved documents

--- backend/core/retrieval/query_vector_db.py
+++ backend/core/retrieval/query_vector_db.py
@@ -1,7 +1,7 @@
 def query_vector_db(query_vector, index, vector_type):
     return index.query(
         vector = query_vector,
-        top_k = 10,
+        top_k = 20,
         include_vectors = False,
         include_metadata = True,
         filter=f"type = '{vector_type}'"  # Upstash metadata filter syntax
